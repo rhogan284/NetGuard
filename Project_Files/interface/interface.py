3f82f6b14b0f02012621002ab0af39ba676ef00c
Gene <e.you8655@gmail.com>
2024-10-18 14:59:37 +1100

    idk
    attempt at getting the interface to work as a Docker Image (it didn't work)

--- Project_Files/interface/interface.py
+++ Project_Files/interface/interface.py
@@ -53,4 +53,4 @@ def internal_error(error):
     return render_template('error.html'), 500
 
 if __name__ == '__main__':
-    app.run(port=6000, debug=True)
+    app.run(host = '0.0.0.0', port=5123, debug=True)
